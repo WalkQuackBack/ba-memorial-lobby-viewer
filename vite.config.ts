bd98934c0de6e8d59ade45e71c80b8f1bf6713ae
WalkQuackBack <quacksareback@duck.com>
2026-01-21 20:39:43 -0500

    build: only target ES module

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig(({ mode }) => {
       lib: isDemoBuild ? false : {
         entry: './src/index.ts',
         name: 'BaMemorialLobbyViewer',
-        fileName: (format) => `ba-memorial-lobby-viewer.${format}.js`,
-        formats: ['es', 'umd', 'cjs'],
+        fileName: 'ba-memorial-lobby-viewer',
+        formats: ['es'],
       },
       rollupOptions: {
         external: ['lit', '@esotericsoftware/spine-webcomponents']
