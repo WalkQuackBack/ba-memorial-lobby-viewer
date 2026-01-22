49151dcd76ed2e5e7956ec0498d97ec0757b4eb8
WalkQuackBack <quacksareback@duck.com>
2026-01-21 20:36:45 -0500

    feat: add custom-elements.json generation and disable build minification

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,16 @@ export default defineConfig(({ mode }) => {
       assetsDir: 'src',
       target: ['es2015'],
       cssMinify: true,
+      minify: false,
       lib: isDemoBuild ? false : {
         entry: './src/index.ts',
         name: 'BaMemorialLobbyViewer',
         fileName: (format) => `ba-memorial-lobby-viewer.${format}.js`,
         formats: ['es', 'umd', 'cjs'],
       },
+      rollupOptions: {
+        external: ['lit', '@esotericsoftware/spine-webcomponents']
+      }
     },
   }
 })
